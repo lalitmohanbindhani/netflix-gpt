ee446b0b67aeac54baf47a227287095ceeb71806
Lalit Mohan Bindhani <bindhanilalitmohan@gmail.com>
2025-08-21 10:43:18 +0530

    sign in/ sign up && login/header

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,15 +1,51 @@
+import { useState } from "react";
 import Header from "./Header";
 
 const Login = () => {
+  const [isSignInForm, setIsSignInForm] = useState(true);
+
+  const tougleSignInForm = () => {
+    setIsSignInForm(!isSignInForm);
+  };
   return (
     <div>
       <Header />
-      <div>
+      <div className="absolute">
         <img
           src="https://assets.nflxext.com/ffe/siteui/vlv3/258d0f77-2241-4282-b613-8354a7675d1a/web/IN-en-20250721-TRIFECTA-perspective_cadc8408-df6e-4313-a05d-daa9dcac139f_large.jpg"
           alt="logo"
         />
       </div>
+      <form className="w-3/12 absolute p-12 bg-black my-36 mx-auto right-0 left-0 text-white rounded-lg bg-opacity-80">
+        <h1 className="font-bold text-3xl py-3">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </h1>
+        {!isSignInForm && (
+          <input
+            type="text"
+            placeholder="Full Name"
+            className="p-3 my-3 w-full bg-gray-700 bg-opacity-40"
+          />
+        )}
+        <input
+          type="text"
+          placeholder="Email Address"
+          className="p-3 my-3 w-full bg-gray-700 bg-opacity-40"
+        />
+        <input
+          type="password"
+          placeholder="Password"
+          className="p-3 my-3 w-full bg-gray-700 bg-opacity-40"
+        />
+        <button className="p-4 my-4 bg-red-700 w-full rounded-md">
+          {isSignInForm ? "Sign In" : "Sign Up"}
+        </button>
+        <p className="py-4 cursor-pointer" onClick={tougleSignInForm}>
+          {isSignInForm
+            ? "New to Netflix ? Sign Up"
+            : "Already Registred ? Sign In"}
+        </p>
+      </form>
     </div>
   );
 };
