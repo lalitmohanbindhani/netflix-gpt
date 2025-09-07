e1137134608171a9fd608be4361b5b9e9df38096
Lalit Mohan Bindhani <bindhanilalitmohan@gmail.com>
2025-09-07 10:57:17 +0530

    changes main container

--- src/components/MainContainer.js
+++ src/components/MainContainer.js
@@ -5,13 +5,13 @@ import VideoBackground from "./VideoBackground";
 const MainContainer = () => {
   const movies = useSelector((store) => store.movies?.nowPlayingMovies);
   if (!movies) return;
-  const mainMovie = movies[2];
+  const mainMovie = movies[6];
   console.log(mainMovie);
-  const { original_title, overview } = mainMovie;
+  const { original_title, overview, id } = mainMovie;
   return (
     <div>
       <VideoTitle title={original_title} overview={overview} />
-      <VideoBackground />
+      <VideoBackground movieId={id} />
     </div>
   );
 };
--- src/components/VideoBackground.js
+++ src/components/VideoBackground.js
@@ -1,5 +1,38 @@
-const VideoBackground = () => {
-  return <div>VideoBackground</div>;
+import { useEffect } from "react";
+import { API_OPTIONS } from "../utils/constants";
+
+const VideoBackground = ({ movieId }) => {
+  const getMovieVideos = async () => {
+    const data = await fetch(
+      "https://api.themoviedb.org/3/movie/1175942/videos?language=en-US",
+      API_OPTIONS
+    );
+    const json = await data.json();
+    console.log(json);
+
+    const filterDate = json.results.filter((Video) => Video.type === "Trailer");
+    const trailer = filterDate.length ? filterDate[0] : json.results[0];
+    console.log(trailer);
+  };
+
+  useEffect(() => {
+    getMovieVideos();
+  }, []);
+
+  return (
+    <div>
+      <iframe
+        width="560"
+        height="315"
+        src="https://www.youtube.com/embed/HvLHYox_Vq8?si=CfT7-4CC_bl8jxB_"
+        title="YouTube video player"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
 };
 
 export default VideoBackground;
